394f291386e23dd2fa902d82699a9eccee490c9c
zostaw <zostaw@users.noreply.github.com>
2025-01-05 13:07:23 +0100

    Working top-down, but with no trimming, so hyper inefficient

--- racket/WIP-top-down.rkt
+++ racket/WIP-top-down.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-(require syntax/parse)
-
   #|
    Grow list of programs by composing them from grammar tree.
    It's analogical to *bottom-up-explicit-search.rkt* implementation,
@@ -28,6 +26,8 @@
 
 
   This implementation is not super efficient, it's mostly for learning purposes. I know for sure that it could be optimized at least within the range of one order, because I sometimes search multiple times for something that I could technically do in one run.
+
+  In fact, depth 6 is unreachable basically. No wonder we only remove dupliactes at this point. But I wanna actually add type checking.
   |#
 
 
@@ -36,34 +36,88 @@
 
 
 ;; Terminals evaluate directly to value, non-terminals are dependent on other expressions
-(define terminals (list (Expr 'var #f) (Expr 'one #f)))
-(define non-terminals (list (Expr 'add (list (Expr #f #f) (Expr #f #f))) (Expr 'mul (list (Expr #f #f) (Expr #f #f)))))
+;(define terminals (list (Expr 'var #f) (Expr 'one #f)))
+(define terminals
+  (list (Expr 'const 'terminator)
+        (Expr 'var 'terminator)
+        (Expr 'one 'terminator)
+        (Expr 'op-mul 'terminator)
+        (Expr 'op-add 'terminator)
+        (Expr 'op-sub 'terminator)))
+(define non-terminals
+  (list #;(Expr 'add (list (Expr #f #f) (Expr #f #f)))
+        #;(Expr 'mul (list (Expr #f #f) (Expr #f #f)))
+        (Expr 'arith-expr (list (Expr #f #f) (Expr #f #f) (Expr #f #f)))
+        (Expr 'map (list (Expr #f #f) (Expr #f #f)))
+        (Expr 'function (list (Expr #f #f) (Expr #f #f)))))
 
 
 
 
 
-(define (evaluator var)
+(define (evaluator const)
   (define (eval expr)
-    (cond
-      [(procedure? expr) (eval (expr) 3)]
-      [else
-       (match expr
-         [(Expr 'var _) var]
-         [(Expr 'one _) 1]
-         [(Expr 'add (list left right)) (+ (eval left)
-                                           (eval right))]
-         [(Expr 'sub (list left right)) (- (eval left)
-                                           (eval right))]
-         [(Expr 'mul (list left right)) (* (eval left)
-                                           (eval right))]
-         ;; Those below are really just placeholders at this point, just to keep an eye on them.
-         [(Expr 'map (list fun lst)) (map (eval fun) (eval lst))]
-         [(Expr 'lambda (list fun args)) ((eval fun) (eval args))])]))
+    (match expr
+      ;[(? list? expr) (begin (printf "here list ~a" expr) (eval (map (λ (el) (eval el)) expr)))]
+      [(Expr 'const _) const]
+      
+      [(Expr 'var val) val]
+      
+      [(Expr 'one _) 1]
+      
+      [(Expr 'arith-expr (list arith-op expr-left expr-right))
+             ((eval arith-op) (eval expr-left) (eval expr-right))]
+      
+      [(Expr 'op-add _) +]
+      
+      [(Expr 'op-mul _) *]
+
+      [(Expr 'op-sub _) -]
+      
+      ;; Those below are really just placeholders at this point, just to keep an eye on them.
+      ;[(Expr 'map (list fun lst)) (map (eval fun) (eval lst))]
+      [(Expr 'map (list fun lst)) (eval (map (eval fun) (eval lst)))]
+      [(Expr 'function (list expr-b)) (let ([var-expr-positions (find-var-expr expr-b)])
+                                        (λ (arg)
+                                        (begin
+                                          #;(println "here????\n\n\n")
+  
+                                          (define expr-c (foldl (λ (var-expr-pos transformed-expr)
+                                                                  #;(printf "transformed-expr: ~a\nexpr-b: ~a" transformed-expr expr-b)
+                                                                  (new-expr (if (null? transformed-expr)
+                                                                                expr-b
+                                                                                transformed-expr)
+                                                                            var-expr-pos
+                                                                            (Expr 'var arg)))
+                                                                '()
+                                                                var-expr-positions))
+                                           #;(printf "\nexpression b: [~a], c: [~a]  =eval>  [~a]\n" expr-b expr-c (eval expr-c))
+                                           (eval expr-c))))]
+      [val val]))
   eval)
 
 
-
+(define (find-var-expr expr)
+  
+  (define (parse-path expr node-trace)
+    (match expr
+      [(Expr 'var _) (reverse node-trace)]
+      [(Expr _ 'terminator) '()]
+      [(Expr 'function _) '()]
+      [(Expr _ nodes) (unwrap (filter (λ (lst) (not (empty? lst)))
+                              (for/fold ([acc '()])
+                                        ([id (length nodes)]
+                                         [node nodes])
+                                (cons (parse-path node
+                                                  (cons id node-trace))
+                                      acc))))]
+      [else '()]))
+  
+  (if (Expr-args expr)
+      (filter (λ (x)
+                (not (null? x)))
+              (parse-path expr '()))
+      '()))
 
 
 (define (synthesize inputs outputs #:max-depth [max-depth 2])
@@ -80,19 +134,23 @@
    (lambda (return)
      (for ([current-depth (in-range max-depth)])
        (set! plist (grow plist))
+       (set! plist (remove-duplicates plist))
        ;(set! plist (elim-equivalents plist inputs))
-       #;(for ([program plist])
+       #;(printf "terminated: ~a\n" (filter (λ (p)
+                                            (null? (flatten (find-empty-exprs p))))
+                                          plist))
+       (for ([program (filter (λ (p)
+                                (null? (flatten (find-empty-exprs p))))
+                              plist)])
          (when (is-correct program inputs outputs)
            (return program)))
      (when (equal? (sub1 max-depth) current-depth)
-         (return plist))))))
+         (return #f))))))
 
 
 
 
-
-(define (grow plist)
-  (define (new-expr parent-expr path expr-appendix)
+(define (new-expr parent-expr path expr-appendix)
     (let ([out
            (let ([type (Expr-type parent-expr)]
                  [args (Expr-args parent-expr)])
@@ -109,6 +167,7 @@
       ;(printf "debug out: ~a\n" out)
       out))
 
+(define (grow plist)
     
   (define vocab (append terminals non-terminals))
   ;(printf"\n\n\nnew generation...\n")
@@ -135,12 +194,25 @@
     outcome))
 
 
-
+(define (remove-duplicates plist)
+  (cond
+    [(empty? plist) '()]
+    [(equal? 1 (length plist)) plist]
+    [else (for/fold ([acc '()])
+                    ([i (range (length plist))]
+                     [item-a plist])
+            (cond
+              [(null? acc) (cons item-a acc)]
+              [(ormap (λ (item-b) (equal? item-a item-b)) acc)
+                acc]
+              [else   (cons item-a acc)]))]))
 
 
 (define (is-correct program inputs outputs)
   (define eval (evaluator inputs))
-  (equal? (eval program) outputs))
+  (equal? (with-handlers ([exn:fail? (λ (e) #f)])
+            (eval program))
+          outputs))
 
 
 
@@ -170,10 +242,16 @@
                                          [node nodes])
                                 (cons (parse-path node
                                                   (cons id node-trace))
-                                      acc))))]))
+                                      acc))))]
+      [else '()]))
+  
   (if (Expr-args expr)
-      (parse-path expr '())
+      (filter (λ (x)
+                (not (null? x)))
+              (parse-path expr '()))
       '()))
+
+
 
 
 
@@ -186,22 +264,51 @@
 ###########################################")
   (displayln "\nTest find-empty-exprs:")
   (find-empty-exprs (Expr 'add
-                        (list
-                         (Expr 'var 'terminator)
-                         (Expr 'mul
-                               (list
-                                (Expr 'add
-                                      (list
-                                       (Expr 'one 'terminator)
-                                       (Expr 'one #f)
-                                       (Expr 'mul (list
-                                                   (Expr 'one 'terminator)
-                                                   (Expr 'var #f)))))
-                                (Expr 'var 'terminator))))))
+                          (list
+                           (Expr 'var 'terminator)
+                           (Expr 'mul
+                                 (list
+                                  (Expr 'add
+                                        (list
+                                         (Expr 'one 'terminator)
+                                         (Expr 'one #f)
+                                         (Expr 'mul (list
+                                                     (Expr 'one 'terminator)
+                                                     (Expr 'var #f)))))
+                                  (Expr 'var 'terminator))))))
   (let ([input (list 1 2 3)]
-        [output (list 1 2)]
-        [max-depth 3])
-    (displayln "\nTest synthesize:")
+        [output (list 1 2 3)]
+        [max-depth 2])
+    (printf "\nTest synthesize [~a -> ~a]:\n" input output)
+    (synthesize input output #:max-depth max-depth))
+
+  
+  (let ([input (list 1 2 3)])
+    (let ([eval (evaluator input)]
+          [expr (Expr 'map
+                      (list (Expr 'function
+                                  (list
+                                   (Expr 'arith-expr
+                                         (list (Expr 'op-add 'terminator)
+                                               (Expr 'one 'terminator)
+                                               (Expr 'var 'terminator)))))
+                                        
+                            (Expr 'const 'terminator)))])
+      (printf "\nTest eval [~a]:\n" expr)
+      (eval expr)))
+
+  
+  (let ([input 1]
+        [output 2]
+        [max-depth 4])
+    (printf "\nTest synthesize [~a -> ~a]:\n" input output)
+    (synthesize input output #:max-depth max-depth))
+  
+
+  #;(let ([input (list 1 2 3)]
+        [output (list 2 3 4)]
+        [max-depth 6])
+    (printf "\nTest synthesize [~a -> ~a]:\n" input output)
     (synthesize input output #:max-depth max-depth))
 
   )
